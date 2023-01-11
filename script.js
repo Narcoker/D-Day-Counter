f99830c211a9e08bbc6b942df8e95c827b069be6
Narcoker <jykim9868@gmail.com>
2023-01-11 18:54:20 +0900

    🚑 fix: 메세지와 카운터 중 하나만 보이도록 수정 예외 시간대 입력 시 카운터와 메세지가 둘다 출력되어 하나만 보이도록 수정

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const container = document.querySelector("#d-day-container");
 container.style.display = 'none';
 messageContainer.innerHTML = '<h3>D-Day를 입력해주세요.</h3>';
 
-
 const dateFormMaker = function () {
     const inputYear = document.querySelector("#target-year-input").value;
     const inputMonth = document.querySelector("#target-month-input").value;
@@ -18,15 +17,37 @@ const counterMaker = function () {
     const nowDate = new Date();
     const targetDate = dateFormMaker();
     const remaining = (targetDate - nowDate) / 1000;
+
     if (remaining <= 0) {
         messageContainer.innerHTML = "<h3>타이머가 종료되었습니다.</h3>";
-    } else if (isNaN(remaining)) {
-        messageContainer.innerHTML = "<h3>유효한 시간대가 아닙니다.</h3>";
+        return;
     }
-    const remainingDate = Math.floor(remaining / 3600 / 24);
-    const remainingHours = Math.floor(remaining / 3600 % 24);
-    const remainingMin = Math.floor(remaining / 60) % 60;
-    const remainingSec = Math.floor(remaining) % 60;
 
-    console.log(remainingDate, remainingHours, remainingMin, remainingSec); // Date는 연산이 가능, 결과값은 밀리세컨드
+    if (isNaN(remaining)) {
+        messageContainer.innerHTML = "<h3>유효한 시간대가 아닙니다.</h3>";
+        messageContainer.style.display = 'flex';
+        container.style.display = 'none';
+        return;
+    }
+
+    const remainingObj = {
+        remainingDate: Math.floor(remaining / 3600 / 24),
+        remainingHours: Math.floor(remaining / 3600 % 24),
+        remainingMin: Math.floor(remaining / 60) % 60,
+        remainingSec: Math.floor(remaining) % 60,
+    }
+
+    const documentArr = ['days', 'hours', 'min', 'sec'];
+    const timeKeys = Object.keys(remainingObj);
+
+    let i = 0;
+    for (let tag of documentArr) {
+        document.getElementById(tag).textContent = remainingObj[timeKeys[i++]]
+    }
+}
+
+const starter = function () {
+    container.style.display = 'flex';
+    messageContainer.style.display = "none";
+    counterMaker();
 }
